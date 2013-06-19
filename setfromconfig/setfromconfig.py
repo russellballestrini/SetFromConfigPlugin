a036e89dc948008a39b2ca7619bfd4fdfc82693b
russellballestrini <russell.ballestrini@gmail.com>
2013-06-19 17:07:23 -0400

    thinking about removing the single panel enum commands and having just one command to rule them all.  need to delete block ocmments if this sticks. TODO: We have Error if section is not present, we need to gracefully handle if option is not present.

--- setfromconfig/setfromconfig.py
+++ setfromconfig/setfromconfig.py
@@ -28,7 +28,6 @@ Valid options for this section:
 All other options will be ignored.
 """
 
-
 class SetFromConfigAdminCommandProvider(Component):
     implements(IAdminCommandProvider)
    
@@ -44,51 +43,73 @@ class SetFromConfigAdminCommandProvider(Component):
         #yield ('command regex', '<arg>',
         #       'trac-admin help text',
         #       self.tab_complete_callback, self.command_callback)
-        yield ('priority set from config', None,
-               'Set ticket priorities from config',
-               None, self._set_priority_from_config)
-        yield ('severity set from config', None,
-               'Set ticket severities from config',
-               None, self._set_severity_from_config)
-        yield ('resolution set from config', None,
-               'Set ticket resolutions from config',
-               None, self._set_resolution_from_config)
-        yield ('ticket_type set from config', None,
-               'Set ticket types from config',
-               None, self._set_ticket_type_from_config)
-        #yield ('component set from config', None,
-        #       'Set components from config',
-        #       None, self._set_component_from_config)
-        #yield ('set all from config', None,
-        #       'set all from config',
-        #       None, self._set_all_from_config)
+        ######yield ('priority set from config', None,
+        ######       'Set ticket priorities from config',
+        ######       None, self.set_priority_from_config)
+        ######yield ('severity set from config', None,
+        ######       'Set ticket severities from config',
+        ######       None, self.set_severity_from_config)
+        ######yield ('resolution set from config', None,
+        ######       'Set ticket resolutions from config',
+        ######       None, self.set_resolution_from_config)
+        ######yield ('ticket_type set from config', None,
+        ######       'Set ticket types from config',
+        ######       None, self.set_ticket_type_from_config)
+        ######yield ('component set from config', None,
+        ######       'Set components from config',
+        ######       None, self.set_component_from_config)
+        yield ('set all from config', None,
+               'set all from config',
+               None, self.set_all_from_config)
 
     # the following functions set various enums from config (trac.ini)
-  
-    def _set_priority_from_config(self):
-        panel = PriorityAdminPanel(self.env)
-        self._set_enum_from_config(panel)
-     
-    def _set_severity_from_config(self):
-        panel = SeverityAdminPanel(self.env)
-        self._set_enum_from_config(panel)
-     
-    def _set_resolution_from_config(self):
-        panel = ResolutionAdminPanel(self.env)
-        self._set_enum_from_config(panel)
-     
-    def _set_ticket_type_from_config(self):
-        panel = TicketTypeAdminPanel(self.env)
-        self._set_enum_from_config(panel)
+
+    def get_panels(self):
+        """return a dict of panels"""
+        return {
+          'priority':    PriorityAdminPanel(self.env),
+          'severity':    SeverityAdminPanel(self.env),
+          'resolution':  ResolutionAdminPanel(self.env),
+          'ticket_type': TicketTypeAdminPanel(self.env),
+          'component':   ComponentAdminPanel(self.env),
+        } 
  
-    #def _set_component_from_config(self):
-    #    panel = ComponentAdminPanel(self.env)
-    #    current_items = panel.get_component_list()
-    #                 #panel._do_add(config_item,section.get('component_owner'))
-    #    changes = self._set_enum_from_config('component', panel)
-    #    if changes: 
-    #        printout(json.dumps({'changed':True, 'comment':changes}))
+   ###### def set_priority_from_config(self):
+   ######     panel = PriorityAdminPanel(self.env)
+   ######     self._set_enum_from_config(panel)
+   ######  
+   ###### def set_severity_from_config(self):
+   ######     panel = SeverityAdminPanel(self.env)
+   ######     self._set_enum_from_config(panel)
+   ######  
+   ###### def set_resolution_from_config(self):
+   ######     panel = ResolutionAdminPanel(self.env)
+   ######     self._set_enum_from_config(panel)
+   ######  
+   ###### def set_ticket_type_from_config(self):
+   ######     panel = TicketTypeAdminPanel(self.env)
+   ######     self._set_enum_from_config(panel)
  
+    def set_all_from_config(self):
+        
+        # keep track of changes
+        changes = {}
+        changed = False
+
+        panels = self.get_panels()
+       
+        for name,panel in panels.items():
+            if name == "component":
+                change = self._set_component_from_config(panel)
+            else:
+                change = self._set_enum_from_config(panel)
+            if change:
+                changes[name] = change
+                changed = True
+
+        if changed:
+            printout(json.dumps({'changed':changed, 'comment':changes}))
+
 
     def _get_config_items(self, option_name):
         """
@@ -106,7 +127,8 @@ class SetFromConfigAdminCommandProvider(Component):
         return self.config[self.section_name].getlist(option_name)
 
            
-    def _set_enum_from_config(self, panel, stdout=True):
+    #def _set_enum_from_config(self, panel, stdout=True):
+    def _set_enum_from_config(self, panel):
         """
         Accept panel object
         Fetch config_items from config (trac.ini)
@@ -138,8 +160,43 @@ class SetFromConfigAdminCommandProvider(Component):
                 panel._do_add(config_item)
                 changes[config_item] = 'Added'
         
-        if stdout and changes: 
-            printout(json.dumps({'changed':True, 'comment':changes}))
+        #if stdout and changes: 
+        #    printout(json.dumps({'changed':True, 'comment':changes}))
  
+        return changes
+
+    def _set_component_from_config(self, panel):
+        """
+        same as _set_enum_from_config but handle the special component case
+        components have owners and a different method to get list of items
+        """
+        # config comonenets
+        config_items = self._get_config_items('component')
+
+        # component_owner
+        component_owner = self.config[self.section_name].get('component_owner')
+
+        # database comonenets
+        current_items = panel.get_component_list()
+
+        # keep track of what we change
+        changes = {}
+
+        # remove items from database not present in config (trac.ini)
+        for current_item in current_items:
+            if current_item not in config_items:
+                panel._do_remove(current_item) 
+                changes[current_item] = 'Removed'
+   
+        # add items from config (trac.ini) not present in database
+        for config_item in config_items:
+            if config_item not in current_items:
+                panel._do_add(config_item, component_owner)
+                changes[config_item] = 'Added'
+        
+        #if stdout and changes: 
+        #if changes: 
+        #    printout(json.dumps({'changed':True, 'comment':changes}))
+
         return changes
 
