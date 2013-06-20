312b47ee8d9748ddd29d390ee96ecc3c9d949b67
Travis Fischer <tfischer@cars.com>
2013-06-19 18:05:19 -0700

    Added test scaffolding

---
+++ setfromconfig/tests/__init__.py
@@ -0,0 +1,1 @@
+from .setfromconfig import *
---
+++ setfromconfig/tests/setfromconfig.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+
+
+class SetFromConfigTestCase(unittest.TestCase):
+
+    def test_something(self):
+        self.assertTrue(True)
